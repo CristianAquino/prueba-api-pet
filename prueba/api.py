a00ce6fe052b2edb512a8bd295d3893ba24540d1
cristianaquino <ycristian10@gmail.com>
2022-07-22 15:58:04 -0500

    .

--- prueba/api.py
+++ prueba/api.py
@@ -22,21 +22,14 @@ from .schema import GetProfileSchema, MessageSchema, ResponsePutSchema,ProfileUs
 
 router = Router()
 
-@router.get('/', auth=None, response=List[GetProfileSchema])
-def get_all(request):
-    profile = ProfileUser.objects.all()
-    if len(profile)==0:
-        return MessageSchema
-    else:
-        return profile
-
-# @router.get('/{id}',auth=None, response={200: GetProfileSchema, 404: MessageSchema})
-# def get_one(request, id: int):
-#     try:
-#         profile = ProfileUser.objects.get(pk=id)
-#         return 200, profile
-#     except ProfileUser.DoesNotExist:
-#         return 404, {'message': 'Datos no encontrados'}
+@router.get('/{id}',auth=None, response={200: GetProfileSchema, 404: MessageSchema})
+def get_one(request, id: int):
+    try:
+        profile = ProfileUser.objects.get(pk=id)
+        return 200, profile
+    except ProfileUser.DoesNotExist:
+        return 404, {'message': 'Datos no encontrados'}
+    
 @router.get('/{id}',auth=None, response=List[GetProfileSchema])
 def get_one(request, id: int):
     try:
